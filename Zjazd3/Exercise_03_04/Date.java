0d74657c6070f6dcac2349d1c503fba7fe9e556d
Dorota <dorotamatkowska23@gmail.com>
2019-05-04 18:45:31 +0200

    Zjazd_3

---
+++ Zjazd3/Exercise_03_04/Date.java
@@ -0,0 +1,54 @@
+package Exercise_03_04;
+
+/*
+Dorota Matkowska
+zadanie 4
+*/
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+
+    public Date (int day, int month, int year){
+        this.day = day;
+        this.month=month;
+        this.year=year;
+    }
+    public int getday (){
+        return day;
+    }
+    public int getmonth(){
+        return month;
+    }
+    public int getyear(){
+        return year;
+    }
+    public void setday(int day){
+        this.day = day;
+    }
+    public void setmonth(int month){
+        this.month = month;
+    }
+    public void setyear(int year){
+        this.year = year;
+    }
+    public void setDate(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString(){
+        String dzien = Integer.toString(day);
+        String miesia = Integer.toString(month);
+        String rok = Integer.toString(year);
+
+        while (dzien.length() < 2) dzien = "0" + dzien;
+        while (miesia.length() < 2) miesia = "0" + miesia;
+        while (rok.length() < 4) rok = "0" + rok;
+
+        return "Date["+dzien+"/"+miesia+"/"+rok+"]";
+    }
+}
---
+++ Zjazd3/Exercise_03_04/Main.java
@@ -0,0 +1,13 @@
+package Exercise_03_04;
+
+/*
+Dorota Matkowska
+zadanie 4
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        Date data = new Date(23, 03, 1994);
+        System.out.println("test wyswietli  " + data.toString());
+    }
+}
