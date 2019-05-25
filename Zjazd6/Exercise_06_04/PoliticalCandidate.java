0227f61ad48cd4a8fc30434e6b40f194d499d922
Dorota Matkowska <43983056+DorotaMatkowska@users.noreply.github.com>
2019-05-25 16:14:34 +0200

    Create PoliticalCandidate.java

---
+++ Zjazd6/Exercise_06_04/PoliticalCandidate.java
@@ -0,0 +1,15 @@
+
+/*
+Autorzy: Aleksander Tyński
+        Dorota Matkowska
+   Zadanie 4 - Runner
+   Lab 6
+*/
+
+
+public class PoliticalCandidate implements Runner {
+    @Override
+    public void run() {
+        System.out.println("Political Candidate runs in elections to become president");
+    }
+}
